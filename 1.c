c3de66e91fc3af59c543ecf3ea0a9ff35550adfc
Kasi-Vishwanathan <coder0110wl@gmail.com>
2025-04-18 18:54:58 +0530

    1.c – demonstrates division by zero (runtime error)

--- 1.c
+++ 1.c
@@ -1,3 +1,10 @@
+/*
+ * @Description: This program performs division but contains a runtime error due to division by zero.
+ * @ErrorType: Runtime Error
+ * @FileName: buggy1.c
+ */
+
+
 #include <stdio.h>
 
 int main() {
